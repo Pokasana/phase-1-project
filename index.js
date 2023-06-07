54ccf04dd017bf2468df88b558004394882e190b
Sanae <sanae.s.soma@gmail.com>
2023-06-07 10:53:01 -1000

    Load movie list on the DOM

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
+    //Fetch
+    fetch('http://localhost:3000/movies')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        data.forEach(item => createMovieList(item));
+    })
+
+    //Functions
+    function createMovieList(movie) {
+        console.log(movie);
+        const container = document.querySelector('.movie-list-container');
+        const ul = document.createElement('ul');
+        const li = document.createElement('li');
+        li.textContent = movie.title;
+        ul.appendChild(li);
+        container.appendChild(ul);
+    };
+
   });
